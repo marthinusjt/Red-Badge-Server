05835eb33a39bb3028defc04f6e7da03459af386
marthinusjt <marthinusjt@gmail.com>
2019-10-16 14:26:16 -0400

    mjt

--- app.js
+++ app.js
@@ -6,18 +6,22 @@ const express = require('express');
 const app = express();
 const review = require('./controllers/reviewcontroller');
 const user = require('./controllers/usercontroller');
-const forum = require('./controllers/forumcontroller');
+const forumTopic = require('./controllers/forumTopicController');
+const forumReply = require('./controllers/forumReplyController');
 
 const sequelize = require('./db');
 
 sequelize.sync();
+// sequelize.sync({force: true}); // tip: {force: true} for resetting tables
+
 app.use(express.json());
 app.use(require('./middleware/headers')); 
 
 
 app.use('/auth', user);
 app.use('/review', review);
-app.use('/forum', forum);
+app.use('/forumTopic', forumTopic);
+app.use('/forumReply', forumReply);
 
 app.use(require('./middleware/validate-session'))
 
